8e8a55af9893d9fbc06c806262cad0cbf6043d16
Jason Quach (Huu Thuan) <thuanqh@gmail.com>
2018-09-10 12:42:12 +0700

    add now-ui-kit theme

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    siteMetadata: {
+      title: 'Lung Vang Landing Page',
+    },
+    plugins: [
+      'gatsby-plugin-react-helmet',
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: 'lung-vang',
+          short_name: 'lv',
+          start_url: '/',
+          background_color: '#663399',
+          theme_color: '#663399',
+          display: 'minimal-ui',
+          icon: 'src/assets/images/lungvang-icon.png', // This path is relative to the root of the site.
+        },
+      },
+      'gatsby-plugin-offline',
+      {
+        resolve: `gatsby-plugin-typography`,
+        options: {
+          pathToConfigModule: `src/utils/typography.js`,
+        },
+      },
+    ],
+  }

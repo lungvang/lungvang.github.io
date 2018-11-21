304d1c975f3db6bbdd0409f6a72a7dce5b1e1dc5
Jason Quach (Huu Thuan) <thuanqh@gmail.com>
2018-11-21 15:46:56 +0700

    Clean bootstrap

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,5 @@
 import Typography from "typography"
-import bootstrapTheme from "typography-theme-bootstrap"
 
-const typography = new Typography(bootstrapTheme)
+const typography = new Typography()
 
 export default typography
